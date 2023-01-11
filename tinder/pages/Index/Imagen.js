f599e62058c61445263db912390e0b69e105c66a
Leandro Echezuri <leandro.echezuri@gmail.com>
2023-01-11 19:26:32 -0300

    register 1 y 2

--- tinder/pages/Index/Imagen.js
+++ tinder/pages/Index/Imagen.js
@@ -2,12 +2,8 @@ import React from "react";
 
 const Imagen = () => {
   return (
-    <div className="max-w-[1000px] mx-auto flex flex-col justify-center">
-      <img
-        className="h-screen bg-yellow-300 w-6/6 mx-auto mb-10  md:mt-12"
-        src="./Music.png"
-        alt="HTML icon"
-      />
+    <div className="bg-fondito bg-center bg-no-repeat h-screen flex flex-col justify-center  m-auto">
+      <img className="w-60 m-auto" src="./Music.png" alt="HTML icon" />
     </div>
   );
 };
---
+++ tinder/pages/register/register.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Cruz from "./cruz.js";
+
+export default function Register() {
+  return (
+    <div>
+      <div>
+        <Cruz />
+      </div>
+      <div>
+        <h2>Mi nombre es</h2>
+        <form>
+          <input type="text" placeholder="Ingresa tu nombre.."></input>
+          <p>Así es como se verá en tu perfil</p>
+          <input type="email" placeholder="Ingresa tu email.."></input>
+          <p>Ingresa tu fecha de nacimiento</p>
+          <input type="date"></input>
+
+          <div>
+            <p>Genero</p>
+            <button value="hombre">Hombre</button>
+            <button value="mujer">Mujer</button>
+            <button value="otro">Otro</button>
+          </div>
+          <div>
+            <p>Mostrar</p>
+            <button value="hombres">Hombres</button>
+            <button value="mujeres">Mujeres</button>
+            <button value="ambos">Ambos</button>
+          </div>
+
+          <button>Continue</button>
+        </form>
+      </div>
+    </div>
+  );
+}
