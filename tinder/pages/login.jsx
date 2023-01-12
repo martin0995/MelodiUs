5d0807900d16223d9f54877f09e0103878a76e1a
elbolaestevez <elbolaestevez@gmail.com>
2023-01-12 04:57:45 -0300

    next-auth-google

---
+++ tinder/pages/login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const login = () => {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <div>
+        <p>Bienvenido,{session.user.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>No estas loguiado</p>
+        <button onClick={() => signIn()}>Sign In</button>
+      </div>
+    );
+  }
+};
+
+export default login;
