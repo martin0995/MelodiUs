929539c35a14c77cc3bc80f3f6d5c3fc127e0470
elbolaestevez <elbolaestevez@gmail.com>
2023-01-17 19:12:29 -0300

    css structure home

--- tinder/pages/logged/home.jsx
+++ tinder/pages/logged/home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { useSession, signOut } from "next-auth/react";
-
+import Icon from "../Index/Icon";
+import Navbar from "../../components/Navbar";
 const home = () => {
   const { data: session, status } = useSession();
 
   if (status === "authenticated") {
     return (
-      <div>
-        <button onClick={() => signOut({ callbackUrl: "/" })}>Sign out</button>
-        <p>Bienvenido,{session.user.name}</p>
+      <div className="bg-white text-black h-screen">
+        <div className="flex flex-row text-verdedos">
+          <div className="p-2 h-8 flex mx-auto gap-1">
+            <Icon />
+            <h6> tinderMusic</h6>
+          </div>
+          {/* <button onClick={() => signOut({ callbackUrl: "/" })}>Logout</button> */}
+        </div>
+        <div className="m-auto relative p-2.5 border-4 border-solid w-11/12 h-heightdiv mt-2">
+          <img className={"inset-0 h-full"}></img>
+        </div>
+        <Navbar></Navbar>
       </div>
     );
   }
--- tinder/tailwind.config.js
+++ tinder/tailwind.config.js
@@ -10,9 +10,12 @@ module.exports = {
         fondito: "url('../public/bg-img3.jpeg')",
       },
       colors: {
-        "rojito": "#e5383b",
-        "verdecito": "#2b9348",
-        "verdedos": "#007f5f"
+        rojito: "#e5383b",
+        verdecito: "#2b9348",
+        verdedos: "#007f5f",
+      },
+      height: {
+        heightdiv: "47rem",
       },
     },
   },
