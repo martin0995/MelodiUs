02b05c2795d7803ebada22bf48412c33afffad64
elbolaestevez <elbolaestevez@gmail.com>
2023-02-09 12:49:14 -0300

    pwa

--- tinder/next.config.js
+++ tinder/next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
 };
+const Pwa = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
-module.exports = nextConfig;
+module.exports = { nextConfig, Pwa };
