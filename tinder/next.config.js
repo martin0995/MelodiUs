517dbe3080f4ecda6c1425da3d6d84bd3f817dc3
elbolaestevez <elbolaestevez@gmail.com>
2023-02-16 15:45:24 -0300

    pwa done

--- tinder/next.config.js
+++ tinder/next.config.js
@@ -2,24 +2,20 @@
 
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    scope: "/app",
-    sw: "service-worker.js",
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
-    disable: process.env.NODE_ENV === "development",
-  },
+  dest: "public",
 });
-
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-};
+// const withPWA = require("next-pwa")({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     scope: "/app",
+//     sw: "service-worker.js",
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest.json$/],
+//     disable: process.env.NODE_ENV === "development",
+//   },
+// });
 
 // const nextConfig = withPWA({
 //   reactStrictMode: true,
@@ -28,4 +24,18 @@ const nextConfig = {
 //   },
 // });
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  nextConfig: {
+    reactStrictMode: true,
+    images: {
+      domains: ["res.cloudinary.com"],
+    },
+  },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    buildExcludes: [/middleware-manifest.json$/],
+    runtimeCaching,
+    register: true,
+  },
+});
