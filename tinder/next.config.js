dc399f9676207c63014b6e5300f886302d9c35bb
elbolaestevez <elbolaestevez@gmail.com>
2023-02-16 16:38:54 -0300

    pwa done

--- tinder/next.config.js
+++ tinder/next.config.js
@@ -3,39 +3,15 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest.json$/],
+  runtimeCaching,
+  register: true,
 });
-// const withPWA = require("next-pwa")({
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//     scope: "/app",
-//     sw: "service-worker.js",
-//     runtimeCaching,
-//     buildExcludes: [/middleware-manifest.json$/],
-//     disable: process.env.NODE_ENV === "development",
-//   },
-// });
-
-// const nextConfig = withPWA({
-//   reactStrictMode: true,
-//   images: {
-//     domains: ["res.cloudinary.com"],
-//   },
-// });
 
 module.exports = withPWA({
-  nextConfig: {
-    reactStrictMode: true,
-    images: {
-      domains: ["res.cloudinary.com"],
-    },
-  },
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    buildExcludes: [/middleware-manifest.json$/],
-    runtimeCaching,
-    register: true,
+  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
   },
 });
